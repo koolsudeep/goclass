da9bd3dc17b4aca1be2b2a9640e1fb49629d9dab
koolsudeep <koolsudeep@gmail.com>
2022-02-03 20:23:01 -0600

    First Commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/koolsudeep/goclass/goroutines"
+)
+
+// var a int8 = -1
+
+// const PI = 3.27
+
+func main() {
+	// initials.Init()
+	// arraypractise.Init()
+	// datastructure.Init()
+	// structure.Init2(5, 2, 5)
+	// structure.Init3()
+	// goroutines.InitErrorStrategy()
+	// goroutines.Init()
+	// goroutines.Init2()
+	// goroutines.InitClosingChannel()
+	goroutines.InitDeferExample()
+
+}
