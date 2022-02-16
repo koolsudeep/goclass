d1a1c385d9589479452dd8f565e58d116b0d6dd4
koolsudeep <koolsudeep@gmail.com>
2022-02-15 21:42:09 -0600

    added files

---
+++ exmp_http/exmp_http_client.go
@@ -0,0 +1,23 @@
+package exmphttp
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func StartServer() {
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("testing"))
+	})
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	if value, ok := req.Header["Accept"]; ok {
+		fmt.Println("header =", value)
+	}
+	w.Write([]byte("Hello"))
+}
